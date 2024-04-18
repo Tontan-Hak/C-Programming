f410a3132d51a4efc2cba98c001a184790245a04
tontanhak97@gmail.com <tontanhak97@gmail.com>
2024-04-18 18:38:26 +0700

    add new exercises

---
+++ hello/input_01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /* code */
+    char op;
+    float n1, n2;
+
+    printf("operation: ");
+    scanf("%c", &op);
+
+    printf("N1: ");
+    scanf("%f", &n1);
+
+    printf("N2:");
+    scanf("%f", &n2);
+
+    return 0;
+}
