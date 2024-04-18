94b39102393ca7ecceb465eac98d54522a3c4092
Tontan Hak <tontanhak97@gmail.com>
2024-04-18 14:53:18 +0700

    run new code testing

---
+++ C/testing4.c
@@ -0,0 +1,12 @@
+// C program to demonstrate 
+// double float keyword
+#include <stdio.h>
+
+// Driver code
+int main() {
+  float f = 0.3;
+  double d = 10.67;
+  printf("Float value: %f\n", f);
+  printf("Double value: %f\n", d);
+  return 0;
+}
---
+++ C/testing6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//int main()
+int main(int argc, char *argv[])
+{
+    /* code */
+    char op;
+    float n1, n2;
+
+    printf("N1: ");
+    scanf(" %f", &n1);
+
+    printf("operation: ");
+    scanf(" %c", &op);
+
+    printf("N2:");
+    scanf("%f", &n2);
+
+    return 0;
+}
