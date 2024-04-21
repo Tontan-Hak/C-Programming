3586720912cc59a32f1571e8a33d1e73b9220909
tontanhak97@gmail.com <tontanhak97@gmail.com>
2024-04-21 11:28:44 +0700

    debug code

--- C/C For Everybody/week3/Ex1.c
+++ C/C For Everybody/week3/Ex1.c
@@ -1,2 +1,10 @@
 #include <stdio.h>
 
+int main(){
+    char line[1000];
+    FILE *hand;
+    hand = fopen("test.txt", "r");
+    while(fgets(line, 1000, hand) != NULL){
+        printf("%s", line);
+    }
+}
