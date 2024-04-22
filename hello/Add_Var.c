7214a04c6818ced2fdd358e3f9ca360f7b472713
Tontan Hak <tontanhak97@gmail.com>
2024-04-22 14:59:10 +0700

    Var and Data type example code

---
+++ hello/Add_Var.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* int main() {
+    int x = 5;
+    int y = 6;
+    int sum = x + y;
+    printf("%d", sum);
+    return 0;
+} */
+/* int main() {
+    int x = 5, y = 6, z = 50;
+    printf("%d", x + y + z);
+    return 0;
+} */
+
+int main() {
+    int x, y, z;
+    x = y = z = 50;
+    printf("%d", x + y + z);
+    return 0;
+}
---
+++ hello/Data_type.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*int main() {
+    double myDoubleNum = 19.99;  // Double (floating point number)
+
+    printf("%lf", myDoubleNum);
+    return 0;
+}*/
+
+// Use ASCII values to print the character
+int main() {
+  char a = 65, b = 66, c = 67;
+  printf("%c\n", a);
+  printf("%c\n", b);
+  printf("%c", c);
+  return 0;
+}
