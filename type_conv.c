0e5b980ebc2e22c1503b7f6a6959c59a54e627f0
Tontan Hak <tontanhak97@gmail.com>
2024-04-22 21:37:01 +0700

    Operator

---
+++ type_conv.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/* int main() {
+  // Set the maximum possible score to 500
+    int maxScore = 500;
+
+  // The actual score of the user
+    int userScore = 420;
+
+  // Calculate the percantage of the user's score in relation to the maximum available score
+  float percentage = (float) userScore / maxScore * 100.0;
+
+  // Print the percentage
+    printf("User's percentage is %.2f", percentage);
+
+    return 0;
+} */
+
+int main() {
+    const int minutesPerHour = 60;
+    const float PI = 3.14;
+
+    printf("%d\n", minutesPerHour);
+    printf("%f\n", PI);
+    return 0;
+}
