1f96d5d51440b5366bb21fb3b1560b0eeed3af26
Tontan Hak <tontanhak97@gmail.com>
2024-04-23 09:02:03 +0700

    If else code testing and learning

---
+++ Intro_C/IF.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+/* int main(){
+    if (23>18) {
+        printf("23 is greater than 18");
+        }
+    else
+        printf("23 is not greater than 18");
+    return 0;
+} */
+
+/* int main() {
+    int time = 22;
+    if (time < 10) {
+        printf("Good morning.");
+    } else if (time < 20) {
+        printf("Good day.");
+    } else {
+        printf("Good evening.");
+    }
+    return 0;
+} */
+
+/*
+int main() {
+    int time = 20;
+    (time < 18) ? printf("Good day.") : printf("Good evening.");
+    return 0;
+}*/
+
+/*
+int main() {
+    int myAge = 25;
+    int votingAge = 18;
+
+    if (myAge >= votingAge) {
+        printf("Old enough to vote!");
+    } else {
+        printf("Not old enough to vote.");
+    }
+    
+    return 0;
+}*/
+
+int main() {
+    int myNum = 100;
+    if (myNum == 100) {
+        printf("%d is even.\n", myNum);
+    } else {
+        printf("%d is odd.\n", myNum);
+    }
+}
